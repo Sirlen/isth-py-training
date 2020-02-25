fb7636db551c1e2f9dae1c03490c2c5fee03a69c
Sirlen <61214766+Sirlen@users.noreply.github.com>
2020-02-25 17:44:17 -0600

    Update file_repository.py

--- app/infrastructure/file_repository.py
+++ app/infrastructure/file_repository.py
@@ -1,10 +1,19 @@
 """Service methods for file endpoints"""
 
+# SCE
+from app.common import Constants
+
+import os
+
 
 class fileRepository:
 
     def get_file(self, id):
         return NotImplementedError
 
-    def post_file(self, data):
+    def post_file(self, nombre, data):
+
+        filepath = os.path.join(FILE_UPLOAD_PATH, nombre) if not os.path.exists(FILE_UPLOAD_PATH): os.makedirs(FILE_UPLOAD_PATH) f = open(filepath, "a")
         return NotImplementedError
+
+        
